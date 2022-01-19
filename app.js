ef1de8501a8f714fe5a2d545895e74602f1bbc33
gusvicenzi <gusvicenzi12@gmail.com>
2022-01-19 17:37:21 -0300

    added encryption to passwords

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 const urlmongo = "mongodb://localhost:27017/userDB";
@@ -12,11 +13,15 @@ if (port == null || port == "") {
 mongoose.connect(urlmongo);
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+  email: String,
+  password: String,
 });
 
-new User = new mongoose.model("User", userSchema);
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -26,27 +31,48 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function (req, res) {
-  res.render("login");
-});
+app
+  .route("/login")
+  .get(function (req, res) {
+    res.render("login");
+  })
+  .post(function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.route("/register")
-.get(function (req, res) {
-  res.render("register");
-})
-.post(function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        }else{
+    User.findOne({ email: username }, function (err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
             res.render("secrets");
+          } else {
+            console.log("Wrong password.");
+          }
         }
+      }
     });
-});
+  });
+
+app
+  .route("/register")
+  .get(function (req, res) {
+    res.render("register");
+  })
+  .post(function (req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
 
 app.listen(port, function () {
   console.log("Successfully started server.");
