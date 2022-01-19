0d5ae1036611a11e6116336c4b9feecc916d187a
gusvicenzi <gusvicenzi12@gmail.com>
2022-01-19 15:55:25 -0300

    add mongo, general routes and create user

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
+const urlmongo = "mongodb://localhost:27017/userDB";
 let port = process.env.PORT;
-if (port === null || port === "") {
+if (port == null || port == "") {
   port = 3000;
 }
 
+mongoose.connect(urlmongo);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+new User = new mongoose.model("User", userSchema);
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.route("/register")
+.get(function (req, res) {
+  res.render("register");
+})
+.post(function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if (err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    });
+});
 
 app.listen(port, function () {
   console.log("Successfully started server.");
