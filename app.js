c2493beb36176d52da56e8799681739ca84ea095
gusvicenzi <gusvicenzi12@gmail.com>
2022-01-19 19:13:13 -0300

    add salt round and hashin with bcrypt

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 const urlmongo = "mongodb://localhost:27017/userDB";
@@ -35,18 +36,20 @@ app
   })
   .post(function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: username }, function (err, foundUser) {
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          } else {
-            console.log("Wrong password.");
-          }
+          bcrypt.compare(password, foundUser.password, function (err, result) {
+            if (result === true) {
+              res.render("secrets");
+            } else {
+              console.log("Wrong password.");
+            }
+          });
         }
       }
     });
@@ -58,16 +61,18 @@ app
     res.render("register");
   })
   .post(function (req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password),
-    });
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      });
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
     });
   });
 
